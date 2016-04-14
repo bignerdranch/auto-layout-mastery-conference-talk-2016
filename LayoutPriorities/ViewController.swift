3e4de9d82e1e4fe2e6f96e80bf966ad439f3d00a
Stephen Christopher <random4444@gmail.com>
2016-04-14 13:49:13 -0400

    Initial Commit

---
+++ LayoutPriorities/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LayoutPriorities
+//
+//  Created by Stephen Christopher on 2016.04.14.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
